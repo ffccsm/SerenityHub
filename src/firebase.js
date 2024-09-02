b7255d67408c5c3bd5f3fe3a1f9f883df5512a1a
Md. Sohel Hossain <97468966+ffccsm@users.noreply.github.com>
2024-09-02 09:44:17 +0600

    Minmal changes in Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,21 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';  // Import the auth module
 
-// Firebase configuration stored in environment variables
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCKFB3ruOuyJTHx5LxEkyxVAonB7rt03ho",
+  authDomain: "serenity-form.firebaseapp.com",
+  projectId: "serenity-form",
+  storageBucket: "serenity-form.appspot.com",
+  messagingSenderId: "981044454981",
+  appId: "1:981044454981:web:04bbe622d420ef826b252c",
+  measurementId: "G-6CVEQN819D"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
+// Initialize Firestore
 const db = getFirestore(app);
 
 // Initialize Firebase Authentication
